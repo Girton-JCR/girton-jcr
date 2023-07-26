fda01bf5ec9db27492238de98dc473b73be0c5fc
srw73 <sam.r.williams13@gmail.com>
2023-07-26 20:05:23 +0100

    some style updates

--- src/CollegeLife/EventsCalendar/EventsCalendar.tsx
+++ src/CollegeLife/EventsCalendar/EventsCalendar.tsx
@@ -4,19 +4,14 @@ import usePageTitle from '../../usePageTitle';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import iCalendarPlugin from '@fullcalendar/icalendar';
 import FullCalendar from '@fullcalendar/react';
+import './EventsCalendar.css';
 
 function EventsCalendar() {
   usePageTitle('Events Calendar  | Girton JCR');
+
   return (
     <div className="EventsCalendar">
       <h1>Events Calendar</h1>
-      <p>
-        This event calendar will be updated before each term. If you have any
-        queries or suggestions on events you think the JCR should run for
-        undergraduates, please contact the{' '}
-        <a href="mailto:jcr-ents@girton.cam.ac.uk">JCR Events Officer</a>.
-      </p>
-      <p>Check back soon to see the events calendar!</p>
       <FullCalendar
         plugins={[dayGridPlugin, iCalendarPlugin]}
         initialView="dayGridMonth"
@@ -24,7 +19,15 @@ function EventsCalendar() {
           url: 'https://girtonjcr.soc.srcf.net/jcr_calendar.ics',
           format: 'ics',
         }}
+        contentHeight="auto"
+        eventColor="#154732"
       />
+      <p>
+        This event calendar will be updated before each term. If you have any
+        queries or suggestions on events you think the JCR should run for
+        undergraduates, please contact the{' '}
+        <a href="mailto:jcr-ents@girton.cam.ac.uk">JCR Events Officer</a>.
+      </p>
     </div>
   );
 }
