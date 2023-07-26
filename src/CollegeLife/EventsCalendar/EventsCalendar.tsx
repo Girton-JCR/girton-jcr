106a96b5fb631f79d47ed53b5589d7c2ffb4691c
srw73 <sam.r.williams13@gmail.com>
2023-07-26 21:12:13 +0100

    new list view for caledar

--- src/CollegeLife/EventsCalendar/EventsCalendar.tsx
+++ src/CollegeLife/EventsCalendar/EventsCalendar.tsx
@@ -4,23 +4,39 @@ import usePageTitle from '../../usePageTitle';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import iCalendarPlugin from '@fullcalendar/icalendar';
 import FullCalendar from '@fullcalendar/react';
+import listPlugin from '@fullcalendar/list';
+import useWindowDimensions from '../../useWindowDimensions';
 
 function EventsCalendar() {
   usePageTitle('Events Calendar  | Girton JCR');
+  const win = useWindowDimensions();
 
   return (
     <div className="EventsCalendar">
       <h1>Events Calendar</h1>
-      <FullCalendar
-        plugins={[dayGridPlugin, iCalendarPlugin]}
-        initialView="dayGridMonth"
-        events={{
-          url: 'https://girtonjcr.soc.srcf.net/jcr_calendar.ics',
-          format: 'ics',
-        }}
-        contentHeight="auto"
-        eventColor="#154732"
-      />
+      {win.width <= 700 && (
+        <FullCalendar
+          plugins={[listPlugin, iCalendarPlugin]}
+          initialView={'listMonth'}
+          events={{
+            url: 'https://girtonjcr.soc.srcf.net/jcr_calendar.ics',
+            format: 'ics',
+          }}
+          eventColor="#154732"
+        />
+      )}
+      {win.width > 700 && (
+        <FullCalendar
+          plugins={[dayGridPlugin, iCalendarPlugin]}
+          initialView={'dayGridMonth'}
+          events={{
+            url: 'https://girtonjcr.soc.srcf.net/jcr_calendar.ics',
+            format: 'ics',
+          }}
+          eventColor="#154732"
+        />
+      )}
+
       <p>
         This event calendar will be updated before each term. If you have any
         queries or suggestions on events you think the JCR should run for
